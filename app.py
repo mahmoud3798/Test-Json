fa3ce3df0ca6ff03af96ec0e5fd5ee6675c5ed7e
mahmoud3798 <69508168+mahmoud3798@users.noreply.github.com>
2020-08-15 00:14:39 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('KNN_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='Activity will be {}'.format(output))
+
+@app.route('/predict-api',methods=['GET'])
+def predict_api():
+    '''
+    For rendering results on HTML GUI
+    '''
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    final_features = [np.array([lat,lng])]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
